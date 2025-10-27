90b5ee701e57bdc9cffcf84d57679097b9bec2cf
ExoticSliceOfPizza <136937744+PizzaLvr49@users.noreply.github.com>
2025-10-27 11:01:15 -0400

    run

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,20 @@
 mod panic_handler;
 
 use bevy::prelude::*;
+use bevy::window::{PresentMode, WindowMode};
 use panic_handler::PanicHandler;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
-                present_mode: bevy::window::PresentMode::AutoVsync,
-                mode: bevy::window::WindowMode::BorderlessFullscreen(MonitorSelection::Primary),
+                present_mode: PresentMode::AutoVsync,
+                mode: WindowMode::BorderlessFullscreen(MonitorSelection::Primary),
                 name: Some("Pong".to_string()),
                 ..default()
             }),
             ..default()
         }))
-        .add_plugins(PanicHandler::default());
+        .add_plugins(PanicHandler::default())
+        .run();
 }
